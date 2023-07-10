0e9e8467782bf929c5284694b859589ccd5b25c9
wicho <luismahe2004@gmail.com>
2023-07-10 10:41:28 -0600

    ya se pudo lo del phpmailer

---
+++ contactanos.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title>CONTACTANOS</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="estilo.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    </head>
+
+    <body class="bg-white" style="background-image: url(img/wallpaper.jpg)">
+
+    <nav class="navbar navbar-expand-lg bg-warning bg-gradient row shadow-sm" id="ini" style="width: 100.9%;">
+      <div class="container-fluid">
+
+        <img src="vd_logo.png" alt="" width="110px" class="p-2">
+
+        <button class="navbar-toggler d-md-none d-sm-none d-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <div class="container-fluid">
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <form class="d-flex me-3" role="search">
+
+              <input class="form-control me-2 border border-black rounded-start-pill shadow" id="look" type="search" placeholder="Buscar..." aria-label="Search">
+
+                <button class="btn bg-secondary-subtle border border-black rounded-end-circle rounded-start-0 shadow" type="button" id="button-addon2">
+                  <img src="img/lupa.png" alt="" style="width: 25px">
+                </button>
+
+            </form>
+
+            <div class="collapse navbar-collapse icons" id="navbarSupportedContent">
+                
+                <ul class="navbar-nav">
+
+                    <li class="nav-item p-auto me-1">
+                        <a class="nav-link text-center" aria-current="page" href="prototipo.html">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                            <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
+                          </svg>
+                        </a>
+                    </li>
+
+                    <li class="nav-item p-auto me-1">
+                        <a class="nav-link text-center" aria-current="page" href="carrito.html">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                          </svg>
+                        </a>
+                    </li>
+
+                    <li class="nav-item p-auto it border border-2 border-black shadow-lg">
+                      <a class="nav-link text-center" aria-current="page" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chat-left-text" viewBox="0 0 16 16">
+                          <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                          <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                      </a>
+                  </li>
+                  
+                   
+
+                </ul>
+                
+                <ul class="navbar-nav ms-auto">
+                  <li class="nav-item text-center">
+                    <a class="nav-link" href="#">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                      </svg>
+                    </a>
+                  </li>
+
+
+                  <li class="nav-item text-center">
+                        <a class="nav-link">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                          </svg>
+                        </a>
+                  </li>
+                    
+                </ul>
+
+            </div>
+      </div>
+    </nav>
+
+    <!--este es el formulario-->
+    <form class="bg-warning bg-gradient border border-dark border-3 p-5 rounded-4 w-50 mt-5 mx-auto" action="index.php" method="post">
+
+        <h2 class="text-center titulo_pro text-light"><b>¡ESCRIBENOS!</b></h2>
+        <h4 class="text-center titulo_pro text-dark"><b>Tu opinión nos importa</b></h4>
+        <!--
+        <div class="input-group flex-nowrap w-50">
+            <span class="input-group-text border border-black shadow-sm" id="addon-wrapping">De:</span>
+
+            <input type="text" name="remitente" class="form-control border border-black shadow-sm"" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+        </div>
+        <br/>-->
+
+        <div class="input-group flex-nowrap mb-4 w-100 shadow-sm">
+
+            <span class="input-group-text border border-black bg-dark text-light" id="addon-wrapping"><b style="font-size: 15px">@</b></span>
+            <input type="text" name="destinatario" class="form-control border border-black shadow-sm" placeholder="email@example.com" aria-label="email@example.com" aria-describedby="addon-wrapping">
+
+        </div>
+
+        <div class="input-group mb-4 w-100 shadow-sm">
+
+            <span class="input-group-text border border-black bg-dark text-light" id="inputGroup-sizing-default"><b style="font-size: 10px">ASUNTO</b></span>
+            <input type="text" name="asunto" class="form-control border border-black shadow-sm" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+
+        </div>
+
+        <div class="input-group mb-4 w-100 shadow-sm">
+            <span class="input-group-text border border-black bg-dark text-light"><b style="font-size: 10px">ADD REQUEST</b></span>
+            <textarea class="form-control border border-black" name="mensaje" aria-label="With textarea"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-dark fw-bold rounded-pill border-3 border-white" name="send">SUBMIT</button>
+    </form>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Xykaow5M6xosbl+eovUDxu6Zb+VBzqE3F1fTCepyrViZfmiwD9+vgHMgW8FDoZ2Y" crossorigin="anonymous"></script>
+    
+    </body>
+</html>
