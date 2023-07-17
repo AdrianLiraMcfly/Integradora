df3b27c34128009e51aae97f39a8f6a58cbcd0d9
wicho <luismahe2004@gmail.com>
2023-07-17 12:21:21 -0600

    sSSSSSSss

--- adminView/clientes/index1.php
+++ adminView/clientes/index1.php
@@ -52,7 +52,7 @@
     <nav class="navbar navbar-expand-lg bg-warning bg-gradient row shadow-sm" id="ini" style="width: 100.9%;">
       <div class="container-fluid">
 
-        <img src="vd_logo.png" alt="" width="110px" class="p-2 me-auto">
+        <img src="../../vd_logo.png" alt="" width="110px" class="p-2 me-auto">
 
         <button class="navbar-toggler d-md-none d-sm-none d-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
@@ -67,7 +67,7 @@
             <ul class="navbar-nav me-auto">
 
               <li class="nav-item p-auto me-1">
-                <a class="nav-link text-center" aria-current="page" href="index.php">
+                <a class="nav-link text-center" aria-current="page" href="../../index.php">
                   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
                     <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
                   </svg>
@@ -75,7 +75,7 @@
               </li>
 
               <li class="nav-item p-auto me-1  it border border-2 border-black shadow-lg">
-                <a class="nav-link text-center" aria-current="page" href="carrito.php">
+                <a class="nav-link text-center" aria-current="page" href="../../carrito.php">
                   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                     <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
                   </svg>
@@ -115,7 +115,7 @@
                   <li><a class="dropdown-item" href="sesiones/login.html">Log In</a></li>
                 </ul>      
               </li>
-              
+
             </ul>
 
     <!--
