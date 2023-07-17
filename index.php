4c1b40f107f97b82528e9ae4bb09720c5f50dd1a
wicho <luismahe2004@gmail.com>
2023-07-17 13:24:24 -0600

    jjsjsjsjs

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
                         </svg>
                       </a>
 
-                      <ul class="dropdown-menu border border-black">
+                      <ul class="dropdown-menu bg-body-secondary border border-black border-2">
                         <li><a class="dropdown-item" href="#">Videojuegos</a></li>
                         <li><a class="dropdown-item" href="#">Accesorios</a></li>
                         <li><a class="dropdown-item" href="#">Ropa</a></li>
@@ -85,7 +85,7 @@
                       </svg>
                     </a>
                       
-                    <ul class="dropdown-menu border border-black" style="margin-right: 85px;">
+                    <ul class="dropdown-menu bg-body-secondary border border-black border-2" style="margin-right: 85px;">
                       <li><a class="dropdown-item" href="sesiones/register.html">Sign In</a></li>
                       <li><a class="dropdown-item" href="sesiones/login.html">Log In</a></li>
                     </ul>
