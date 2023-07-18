6662ab956936033cba8d4538782adb302f522f93
Jesus Ramirez <jesusal.rg.03@gmail.com>
2023-07-18 10:24:48 -0600

    corregiones para agregar inventario

--- adminView/productosphp/conectarbd.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user='root';
-$pass='';
-$server='localhost:3308';
-$bd='integradora2';
-$con=mysqli_connect($server,$user,$pass,$bd);
-?>
--- adminView/productosphp/eliminar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include('conectarbd.php');
-    $id=$_GET['id'];
-    $sql="delete from productos where id_producto='$id'";
-    $result=mysqli_query($con,$sql);
-    header('Location: index.php');
-?>
