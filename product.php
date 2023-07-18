065eba51f61f5b600af7c44b19588cbd0ad81ba4
Jorge Luna <jorgefullscout@gmail.com>
2023-07-18 16:28:32 -0600

    Pagina Incio y Producto al 100%

--- product.php
+++ product.php
@@ -54,7 +54,7 @@ $rutaCarpetaImagenes = 'adminView/products/posters/';
                   <ul class="navbar-nav">
   
                       <li class="nav-item p-auto me-1 it border border-2 border-black shadow-lg">
-                          <a class="nav-link text-center" aria-current="page" href="index.html">
+                          <a class="nav-link text-center" aria-current="page" href="index.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
                               <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
                             </svg>
@@ -200,7 +200,7 @@ $rutaCarpetaImagenes = 'adminView/products/posters/';
         <div class="container-fluid bg-dark mt-5 p-4">
 
         <div class="background-categorias" style="background-color: gold; border: 3px white solid;">
-          <h3><b class="titulos-categorias"> <?php echo $persona->categoria ?> </b></h3>
+          <h3><b class="titulos-categorias"> Mas <?php echo $persona->categoria ?> </b></h3>
       </div> 
       <br>
 
@@ -243,7 +243,9 @@ $rutaCarpetaImagenes = 'adminView/products/posters/';
 </div>
 
 
-<?php } ?>
+<?php }
+  $bd = NULL;
+?>
 
             </div>
         </div>
