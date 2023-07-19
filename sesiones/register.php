495636334b077535b23ba267b22a5a4e72e380c7
wicho <luismahe2004@gmail.com>
2023-07-19 10:07:56 -0600

    jdjdj

--- sesiones/register.php
+++ sesiones/register.php
@@ -80,7 +80,7 @@
                     </li>
 
                     <li class="nav-item p-auto me-1">
-                        <a class="nav-link text-center" aria-current="page" href="carrito.php">
+                        <a class="nav-link text-center" aria-current="page" href="../../carrito.php">
                           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                             <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
                           </svg>
@@ -116,9 +116,9 @@
                       </svg>
                     </a>
                       
-                    <ul class="dropdown-menu bg-body-secondary border border-black border-2" style="margin-right: 85px;">
-                      <li><a class="dropdown-item" href="#">Sign In</a></li>
-                      <li><a class="dropdown-item" href="login.html">Log In</a></li>
+                    <ul class="dropdown-menu bg-body-secondary border border-black border-2 p-1" style="margin-right: 85px;">
+                      <li><a class="dropdown-item rounded bg-primary text-light mb-1" href="#"><strong>Sign In</strong></a></li>
+                      <li><a class="dropdown-item rounded" href="login.html">Log In</a></li>
                     </ul>
                         
                   </li>
