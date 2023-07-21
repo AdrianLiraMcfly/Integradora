218a2415837d627c2e8c01eb81a57b757be4d79a
Jesus Ramirez <jesusal.rg.03@gmail.com>
2023-07-21 14:44:00 -0600

    cambios

--- adminView/products/index2.php
+++ adminView/products/index2.php
@@ -71,114 +71,11 @@ $dir = "posters/";
 <body class="bg-white w-100">
   <?php
   include '../encabesado.php';
-  ?>
-    <div class="container-fluid">
-      <div class="row">
-
-        <nav class="col-md-3 col-lg-2 d-md-block sidebar" height="100vh">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-grid-3x3-gap" viewBox="0 0 16 16">
-                    <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z"/>
-                  </svg> 
-                  <strong>Productos</strong>
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="../pedidos/pedidos.php">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-truck-front" viewBox="0 0 16 16">
-                    <path d="M5 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm8 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm-6-1a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H7ZM4 2a1 1 0 0 0-1 1v3.9c0 .625.562 1.092 1.17.994C5.075 7.747 6.792 7.5 8 7.5c1.208 0 2.925.247 3.83.394A1.008 1.008 0 0 0 13 6.9V3a1 1 0 0 0-1-1H4Zm0 1h8v3.9c0 .002 0 .001 0 0l-.002.004a.013.013 0 0 1-.005.002h-.004C11.088 6.761 9.299 6.5 8 6.5s-3.088.26-3.99.406h-.003a.013.013 0 0 1-.005-.002L4 6.9c0 .001 0 .002 0 0V3Z"/>
-                    <path d="M1 2.5A2.5 2.5 0 0 1 3.5 0h9A2.5 2.5 0 0 1 15 2.5v9c0 .818-.393 1.544-1 2v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5V14H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2a2.496 2.496 0 0 1-1-2v-9ZM3.5 1A1.5 1.5 0 0 0 2 2.5v9A1.5 1.5 0 0 0 3.5 13h9a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 12.5 1h-9Z"/>
-                  </svg> Pedidos
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="../clientes/index1.php">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
-                  </svg> Cliente
-                </a>
-              </li>
-
-            </ul>
-        </div>
-      </nav>
-
-    <div class="col-md-9">
-
-
-        <?php if (isset($_SESSION['msg']) && isset($_SESSION['color'])) { ?>
-            <div class="alert alert-<?= $_SESSION['color']; ?> alert-dismissible fade show" role="alert">
-                <?= $_SESSION['msg']; ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-
-        <?php
-            unset($_SESSION['color']);
-            unset($_SESSION['msg']); 
-        } ?>
-
-        <div class="row justify-content-start">
-            <div class="col-auto">
-                <a href="#" class="btn btn-dark rounded-pill mt-3 shadow" data-bs-toggle="modal" data-bs-target="#nuevoModal"><b class="text-warning">AGREGAR PRODUCTOS</b></a>
-            </div>
-        </div>
-
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Nombre del Producto</th>
-                    <th>Precio</th>
-                    <th>Inventario</th>
-                    <th>Categoria</th>
-                    <th>Imagen</th>
-                    <th>Acción</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php while ($row = $peliculas->fetch_assoc()) { ?>
-                    <tr>
-                        <td><?= $row['id_producto']; ?></td>
-                        <td><?= $row['nombre']; ?></td>
-                        <td>$<?= $row['precio']; ?></td> 
-                        <td><?= $row['cantidad']; ?></td>
-                        <td><?= $row['categoria']; ?></td>
-                        <td><img src="<?= $dir . $row['id_producto'] . '.jpg?n=' . time(); ?>" width="100"></td>
-                        <td>
-                            <a href="#" class="btn btn-primary transparent-button" data-bs-toggle="modal" data-bs-target="#editaModal" data-bs-id="<?= $row['id_producto']; ?>"><img src="../iconos/edit-3-svgrepo-com.svg" alt="edit" width="25px"></a>
-
-                            <a href="#" class="btn btn-danger transparent-button" data-bs-toggle="modal" data-bs-target="#eliminaModal" data-bs-id="<?= $row['id_producto']; ?>"><img src="../iconos/trash-svgrepo-com.svg" alt="delate" width="25px"></a>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
-
-
-    <?php
-    $sqlGenero = "SELECT id_categoria, nombre FROM categorias";
-    $generos = $conn->query($sqlGenero);
-    ?>
-
-    <?php include 'nuevoModal.php'; ?>
-
-    <?php $generos->data_seek(0); ?>
-
-    <?php include 'editaModal.php'; ?>
-    <?php include 'eliminaModal.php'; ?>
-
-    </div>
-  </div> 
-  
-
+  ?> 
+    
+<?php
+include 'vistas/listado.php';
+?>
     <script>
         let nuevoModal = document.getElementById('nuevoModal')
         let editaModal = document.getElementById('editaModal')
@@ -192,7 +89,8 @@ $dir = "posters/";
             nuevoModal.querySelector('.modal-body #nombre').value = ""
             nuevoModal.querySelector('.modal-body #descripcion').value = ""
             nuevoModal.querySelector('.modal-body #precio').value = ""
-            nuevoModal.querySelector('.modal-body #categoria').value = ""
+            nuevoModal.querySelector('.modal-body #cantidad').value = ""
+            nuevoModal.querySelector('.modal-body #categoria').value = "" 
             nuevoModal.querySelector('.modal-body #poster').value = ""
         })
 
@@ -200,6 +98,7 @@ $dir = "posters/";
             editaModal.querySelector('.modal-body #nombre').value = ""
             editaModal.querySelector('.modal-body #descripcion').value = ""
             editaModal.querySelector('.modal-body #precio').value = ""
+            editaModal.querySelector('.modal-body #cantidad').value = ""
             editaModal.querySelector('.modal-body #categoria').value = ""
             editaModal.querySelector('.modal-body #img_poster').value = ""
             editaModal.querySelector('.modal-body #poster').value = ""
@@ -213,11 +112,12 @@ $dir = "posters/";
             let inputNombre = editaModal.querySelector('.modal-body #nombre')
             let inputDescripcion = editaModal.querySelector('.modal-body #descripcion')
             let inputPrecio = editaModal.querySelector('.modal-body #precio')
+            let inputCantidad = editaModal.querySelector('.modal-body #cantidad')
             let inputGenero = editaModal.querySelector('.modal-body #categoria')
             let poster = editaModal.querySelector('.modal-body #img_poster')
 
             let url = "getPelicula.php"
-            let formData = new FormData()
+            let formData = new FormData() 
             formData.append('id', id)
 
             fetch(url, {
@@ -230,6 +130,7 @@ $dir = "posters/";
                     inputNombre.value = data.nombre
                     inputDescripcion.value = data.descripcion
                     inputPrecio.value = data.precio
+                    inputCantidad.value = data.cantidad
                     inputGenero.value = data.id_categoria
                     poster.src = '<?= $dir ?>' + data.id_producto + '.jpg'
 
---
+++ adminView/products/nuevaCategoria.php
@@ -0,0 +1,235 @@
+<?php
+
+
+
+session_start();
+
+if (isset($_SESSION['rol']) && $_SESSION['rol'] == 1) {
+
+
+require 'config/database.php'; 
+
+$sqlCategorias = "SELECT * from categorias";
+$Categorias = $conn->query($sqlCategorias);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    
+    <link rel="stylesheet" href="../productos/estilo.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <script src="bootstrap-5.2.3-dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <title>Document</title>
+    <style>
+        /* Personaliza el tamaño del contenedor para centrar verticalmente */
+
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-warning bg-gradient row shadow-sm" id="ini" style="width: 100.9%;">
+      <div class="container-fluid">
+
+        <img src="../productos/vd_logo.png" alt="" width="110px" class="p-2">
+
+        <button class="navbar-toggler d-md-none d-sm-none d-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <div class="container-fluid">
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!--
+            <form class="d-flex me-3" role="search">
+
+              <input class="form-control me-2 border border-black rounded-start-pill shadow" id="look" type="search" placeholder="Buscar..." aria-label="Search">
+
+                <button class="btn bg-secondary-subtle border border-black rounded-end-circle rounded-start-0 shadow" type="button" id="button-addon2">
+                  <img src="../img/lupa.png" alt="" style="width: 25px">
+                </button>
+
+            </form>-->
+
+            <div class="collapse navbar-collapse icons" id="navbarSupportedContent">
+                
+                <ul class="navbar-nav">
+
+                    <li class="nav-item p-auto me-1">
+                        <a class="nav-link text-center" aria-current="page" href="../../index.php">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                            <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
+                          </svg>
+                        </a>
+                    </li>
+
+                    <li class="nav-item p-auto me-1">
+                        <a class="nav-link text-center" aria-current="page" href="../../carrito.php">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                          </svg>
+                        </a>
+                    </li>
+
+
+                    <li class="nav-item dropdown text-center">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-grid-3x3-gap" viewBox="0 0 16 16">
+                          <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z"/>
+                        </svg>
+                      </a>
+
+                      <ul class="dropdown-menu border-black">
+                        <li><a class="dropdown-item" href="#">Videojuegos</a></li>
+                        <li><a class="dropdown-item" href="#">Accesorios</a></li>
+                        <li><a class="dropdown-item" href="#">Ropa</a></li>
+                        <li><a class="dropdown-item" href="#">Juguetes</a></li>
+                        <li><a class="dropdown-item" href="#">Consolas</a></li>
+                        <li><a class="dropdown-item" href="#">Electronica</a></li>
+                      </ul>
+                    </li>
+
+                    <li class="nav-item text-center dropdown p-auto">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg>
+                      </a>
+                      <?php
+                      echo '
+                      <ul class="dropdown-menu border-black">
+                        <li><a class="dropdown-item" href="#">'.$_SESSION['nombre'].'</a></li>
+                        <li><a class="dropdown-item" href="../../src/cerrar_sesion.php">Cerrar Sesion</a></li> 
+                      </ul>
+                    </li>';
+                    ?>
+                    
+                    <li class="nav-item text-center dropdown p-auto it border border-2 border-danger shadow-lg">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-wrench-adjustable" viewBox="0 0 16 16">
+                          <path d="M16 4.5a4.492 4.492 0 0 1-1.703 3.526L13 5l2.959-1.11c.027.2.041.403.041.61Z"/>
+                          <path d="M11.5 9c.653 0 1.273-.139 1.833-.39L12 5.5 11 3l3.826-1.53A4.5 4.5 0 0 0 7.29 6.092l-6.116 5.096a2.583 2.583 0 1 0 3.638 3.638L9.908 8.71A4.49 4.49 0 0 0 11.5 9Zm-1.292-4.361-.596.893.809-.27a.25.25 0 0 1 .287.377l-.596.893.809-.27.158.475-1.5.5a.25.25 0 0 1-.287-.376l.596-.893-.809.27a.25.25 0 0 1-.287-.377l.596-.893-.809.27-.158-.475 1.5-.5a.25.25 0 0 1 .287.376ZM3 14a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
+                        </svg>
+                      </a>
+
+                      <ul class="dropdown-menu border-black">
+                        <li><a class="dropdown-item" href="../products/index2.php">Productos</a></li>
+                        <li><a class="dropdown-item" href="../pedidos/pedidos.php">Pedidos</a></li>
+                        <li><a class="dropdown-item" href="../clientes/index1.php">Clientes</a></li>
+                      </ul>
+                    </li>
+
+                </ul>
+
+            </div>
+      </div>
+    </nav>
+
+    <div class="container">
+        <div class="pt-5 pb-5">
+            <form action="guardarCategoria.php" method="POST" enctype="multipart/form-data" >
+                <div class="form-group">
+                    <label for="categoria_nombre">Nombre</label>
+                    <input class="form-control" type="text" id="nombre" name="nombre"  required>
+                </div>
+                <div class="text-center mt-3">
+                    <button type="submit" class="btn btn-info rounded-pill px-4">Guardar</button>
+                </div>
+            </form> 
+        </div>
+    </div>
+
+    <table class="table table-striped">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Nombre de Categoria</th>
+            <th>Accion</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php while ($row = $Categorias->fetch_assoc()) { ?>
+            <tr>
+                <td><?= $row['id_categoria']; ?></td>
+                <td><?= $row['nombre']; ?></td>
+                <td>
+                    <a href="#" class="btn transparent-button" data-bs-toggle="modal" data-bs-target="#modalEditCategoria" data-bs-id="<?= $row['id_categoria']; ?>"><img src="../iconos/edit-3-svgrepo-com.svg" alt="edit" width="25px"></a>
+
+                    <a href="#" class="btn transparent-button" data-bs-toggle="modal" data-bs-target="#modalEliminarCategoria" data-bs-id="<?= $row['id_categoria']; ?>"><img src="../iconos/trash-svgrepo-com.svg" alt="delate" width="25px"></a>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+include 'pages/modalEditCategoria.php';
+
+include 'pages/modalEliminarCategoria.php';
+
+?>
+
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="./js/evitar_reenvio.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>-->
+    <script>
+        let modalEditCategoria = document.getElementById('modalEditCategoria')
+        let modalEliminarCategoria = document.getElementById('modalEliminarCategoria')
+
+
+
+        modalEditCategoria.addEventListener('hide.bs.modal', event => {
+            editaModal.querySelector('.modal-body #nombre').value = ""
+        })
+
+        modalEditCategoria.addEventListener('shown.bs.modal', event => {
+            let button = event.relatedTarget
+            let id = button.getAttribute('data-bs-id')
+
+            let inputId = modalEditCategoria.querySelector('.modal-body #id')
+            let inputNombre = modalEditCategoria.querySelector('.modal-body #nombre')
+
+
+            let url = "pages/getCategoria.php"
+            let formData = new FormData() 
+            formData.append('id', id)
+
+            fetch(url, {
+                    method: "POST",
+                    body: formData
+                }).then(response => response.json())
+                .then(data => {
+
+                    inputId.value = data.id_categoria
+                    inputNombre.value = data.nombre
+
+                }).catch(err => console.log(err))
+
+        })
+
+        modalEliminarCategoria.addEventListener('shown.bs.modal', event => {
+            let button = event.relatedTarget
+            let id = button.getAttribute('data-bs-id')
+            modalEliminarCategoria.querySelector('.modal-footer #id').value = id
+        })
+    </script>
+
+</body>
+</html>
+
+<?php
+} else {
+  header('location: ../../index.php');
+}
+?>
