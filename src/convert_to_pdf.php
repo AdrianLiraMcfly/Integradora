bcd63f9473e438aba5c8cf212949e291b52293be
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2023-07-25 12:29:12 -0600

    si

--- src/convert_to_pdf.php
+++ src/convert_to_pdf.php
@@ -27,6 +27,53 @@ $dompdf->set_option('isRemoteEnabled', true); // Permite cargar imágenes desde 
 // Renderiza el contenido HTML en PDF
 $dompdf->render();
 
+$pdfward=$dompdf->output();
+
 // Genera el archivo PDF
 $dompdf->stream("Folio.pdf", array("Attachment" => false));
+
+require __DIR__ . 'bootstrap.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+//Load Composer's autoloader
+//require 'vendor/autoload.php';
+
+try 
+{
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                 //Enable SMTP authentication
+    $mail->Username   = 'luismahe2004@gmail.com';            //SMTP username
+    $mail->Password   = 'ebvwebdlbfxeavxs';                 //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;       //Enable implicit TLS encryption
+    $mail->Port       = 465;                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($destinatario, 'Cliente');
+    $mail->addAddress('luismahe2004@gmail.com');     //Add a recipient
+    
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment($pdfward, 'Folio.pdf');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                  //Set email format to HTML
+    $mail->Subject = 'Folio de compra';
+    $mail->Body    = 'Favor de presentar su folio en el local 322 de la plaza de la tecnologia';
+
+    $mail->send();
+    header('Location: ../index.php');
+} 
+catch (Exception $e) 
+{
+    echo "<script>alert('Error al enviar el correo: " . $mail->ErrorInfo . "');</script>";
+}
 ?>
