86be01520ab1c2ab54bcbba1a4cac92a9a36af18
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2023-07-25 16:21:10 -0600

    s

--- src/conexionbd.php
+++ src/conexionbd.php
@@ -7,7 +7,7 @@ $nombrebd='integradora2';
 
 try {
     $bd = new PDO(
-        'mysql:host=54.196.145.118;dbname=' . $nombrebd,
+        'mysql:host=54.209.294.64;dbname=' . $nombrebd,
         $usuario,
         $passw,
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC)
