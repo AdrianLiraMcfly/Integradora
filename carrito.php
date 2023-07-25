8f173ce2deec59d14561b39b592a7bee5ba2c09f
wicho <luismahe2004@gmail.com>
2023-07-25 14:58:23 -0600

    oloAsssshhh

--- carrito.php
+++ carrito.php
@@ -198,7 +198,7 @@ $rutaCarpetaImagenes = 'adminView/products/posters/';
     ?>
 
       <div class="d-flex align-items-center justify-content-center vh-100">
-        <div class="alerts p-2 w-25 text-center rounded-3 shadow-lg">
+        <div class="alerts p-2 w-50 text-center rounded-4 shadow-lg">
 
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
               <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
@@ -209,8 +209,18 @@ $rutaCarpetaImagenes = 'adminView/products/posters/';
               ¡NECESITAS INICIAR SESION PARA USAR EL CARRITO!
             </p>
 
-            <span class="fw-medium"><a class="link-warn" href="sesiones/login.php">¿Ya tienes cuenta?</a></span><br/>
-            <span class="fw-medium"><a class="link-warn" href="sesiones/register.php">¡Crea una!</a></span>
+            <span class="fw-bold bg-warning border border-black border-2 rounded-pill p-1 shadow-sm">
+              <a class="link-warn" href="sesiones/login.php">
+                Inicia Sesión
+              </a>
+            </span>
+            <br/><br/>
+
+            <span class="fw-bold bg-warning border border-black border-2 rounded-pill p-1 shadow-sm">
+              <a class="link-warn" href="sesiones/register.php">
+                ¡Crea una!
+              </a>
+            </span>
         </div>
       </div>
 
