2f8b749e980ada1c74239fb24cde4dd561089e02
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2023-07-26 14:29:50 -0600

    icono

--- busqueda.php
+++ busqueda.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="icon" type="image/png" sizes="32x32" href="icon.png">
   <link rel="stylesheet" href="css/diseno.css">
   <link rel="stylesheet" href="estilo.css">
   <script src="bootstrap-5.2.3-dist/js/bootstrap.min.js"></script>
--- product.php
+++ product.php
@@ -19,7 +19,7 @@ $rutaCarpetaImagenes = 'adminView/products/posters/';
   <title>Producto</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="icon" type="image/png" sizes="32x32" href="icon.png">
   <link rel="stylesheet" href="estilo.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <link rel="stylesheet" href="css/diseno.css">
