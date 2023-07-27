5ea150ab9ee4ae20cae904c32b4e6fe7f2fc5f54
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2023-07-27 13:33:42 -0600

    s

--- sesiones/login.php
+++ sesiones/login.php
@@ -52,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="icon" type="image/png" sizes="32x32" href="../icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/estilo.css">
@@ -174,7 +174,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           ?>
 
             <div class="username mb-5">
-                <input class="border border border-black text-start p-3 shadow-sm rounded w-100" name="email" type="text" required>
+                <input class="border border border-black text-start p-3 shadow-sm rounded w-100" name="email" type="email" required>
                 <label for=""><b>Gmail de usuario</b></label>
             </div>
 
