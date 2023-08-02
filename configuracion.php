4a92a16d4c9b8b2c31fd946bbe2133600df9564b
wicho <luismahe2004@gmail.com>
2023-08-02 14:15:14 -0600

    kiss

--- configuracion.php
+++ configuracion.php
@@ -118,7 +118,7 @@
 
               </div>
       </nav>
-        -->
+      -->
 
       <nav class="navbar navbar-expand-lg bg-warning bg-gradient row shadow-sm navigation-bar-final">
         <div class="container-fluid">
@@ -282,42 +282,34 @@
         </div>
       </nav>
         
-
       <div class="container-fluid w-100">
         <div class="row w-auto p-2">
 
-          <div class="col-md-6 p-2"> <!-- Modificado el tamaño a col-md-6 -->
-            <div class="text-center bg-warning border border-light border-5 rounded rounded-2 p-3 w-auto">
+          <div class="col-6 p-2 d-flex justify-content-center align-items-center">
+            <div class="text-center bg-warning border border-light border-3 rounded rounded-2 p-3 shadow-lg ms-auto me-auto" style="width: fit-content;">
                 <h2 class="wht text-light fw-bold">¿QUÉ DESEAS HACER HOY?</h2>
                 
-                <ul class="list-group w-75 mx-auto">
+                <ul class="list-group w-auto mx-auto p-2">
                   
                   <li class="list-group-item w-auto text-start p-2 border border-0 fw-medium encima rounded-pill mb-2">
+                    <a class="link-con" href="#">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check me-2" viewBox="0 0 16 16">
                         <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
                         <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
                       </svg>
+
                       Cambiar Nombre 
+                    </a>
                   </li>
 
                   <li class="list-group-item w-auto text-start p-2 border border-0 fw-medium encima rounded-pill mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock me-2" viewBox="0 0 16 16">
-                      <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
-                    </svg>Cambiar Contraseña
-                  </li>
-
-                  <li class="list-group-item w-auto text-start p-2 border border-0 fw-medium encima rounded-pill mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people me-2" viewBox="0 0 16 16">
-                      <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
-                    </svg>Cambiar de cuenta
-                  </li>
-
-                  <li class="list-group-item w-auto text-start p-2 border border-0 fw-medium encima rounded-pill">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-history me-2" viewBox="0 0 16 16">
-                      <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z"/>
-                      <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z"/>
-                      <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
-                    </svg>Ver historial de pedidos
+                    <a class="link-con" href="#">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock me-2" viewBox="0 0 16 16">
+                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+                      </svg>
+                      
+                      Cambiar Contraseña
+                    </a>
                   </li>
 
                 </ul>
@@ -326,9 +318,26 @@
           </div>
           
 
-          <div class="col-md-6 text-center">
-              <div class="container-fluid text-light mb-5"><b class="wel">¡BIENVENIDO!</b></div>
+          <div class="col-6 p-2 text-center ms-auto me-auto">
+
+              <div class="container-fluid text-light mt-5 mb-5">
+                <b class="wel">
+                  ¡BIENVENIDO!
+                </b>
+              </div>
               <img src="vd_logo.png" class="wel rounded mx-auto d-block p-2" width="200px" alt="Logo Tienda">
+
+              <div class="container-fluid text-light">
+                <b class="wel2 mb-5">
+                  TU MEJOR OPCIÓN EN VIDEOJUEGOS, CONSOLAS Y ELECTRONICA ...
+                </b>
+              </div>
+
+              <div class="container-fluid text-dark p-2 bg-warning border border-dark border-3 rounded-pill" style="width: fit-content;">
+                <b class="wel3">
+                  DESDE 2010
+                </b>
+              </div>
           </div>
         </div>
       </div>
