9b477e3a28f48c9b44ff24d0cec77b61a9f053e3
wicho <luismahe2004@gmail.com>
2023-08-08 10:38:02 -0600

    seexoo

--- src/modalNombre.php
+++ src/modalNombre.php
@@ -8,18 +8,14 @@
 
         <h5 class="modal-title" id="changeNameLabel">Cambiar Nombre de Usuario</h5>
 
+        <!--
         <button type="button" class="close bg-danger border shadow border-2 border-dark rounded-pill text-light text-center" style="width: 32; height: 32;" data-dismiss="modal" aria-label="Close">
-            <!--
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-backspace" viewBox="0 0 16 16">
-              <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"/>
-            </svg>
-            -->
-
             <span aria-hidden="true" class="fw-bolder" style="font-size: large; width: auto; height: auto;">
               &times;
             </span>
         </button>
-
+        -->
+        
       </div>
 
       <div class="modal-body">
