38978974c59181e5355d60e6132e822bdec785f0
wicho <luismahe2004@gmail.com>
2023-08-08 10:57:19 -0600

    te lloré

--- configuracion.php
+++ configuracion.php
@@ -222,7 +222,6 @@
                   <li class="list-group-item w-auto text-start p-2 border border-0 fw-medium encima rounded-pill mb-2">
                     <button type="button" class="btn" data-toggle="modal" data-target="#changePasswordModal">
 
-                      <span aria-hidden="true">&times;</span>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock me-2" viewBox="0 0 16 16">
                         <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
                       </svg>
