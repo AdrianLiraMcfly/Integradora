ae18cded49f47f65f7a558b36513ac41281a80f6
wicho <luismahe2004@gmail.com>
2023-08-08 11:41:30 -0600

    te lloree

--- configuracion.php
+++ configuracion.php
@@ -202,31 +202,32 @@
         <div class="row w-auto p-2">
 
           <div class="col-6 p-2 d-flex justify-content-center align-items-center">
+
             <div class="text-center bg-warning border border-light border-3 rounded rounded-2 p-3 shadow-lg ms-auto me-auto" style="width: fit-content;">
                 <h2 class="wht text-light fw-bold">¿QUÉ DESEAS HACER HOY?</h2>
                 
-                <ul class="list-group w-auto mx-auto p-2">
+                <ul class="list-group w-auto mx-auto">
                   
-                  <li class="list-group-item w-auto text-start p-2 border border-0 fw-medium encima rounded-pill mb-2">
-                      <button type="button" class="btn" data-toggle="modal" data-target="#changeNameModal">
+                  <li class="list-group-item border border-0 fw-medium encima rounded-pill mb-2">
+                    <button type="button" class="btn w-100 h-100 rounded-pill border border-0" data-toggle="modal" data-target="#changeNameModal">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check me-2" viewBox="0 0 16 16">
-                          <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
-                          <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
-                        </svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person-check" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
+                        <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
+                      </svg>
 
-                        Cambiar Nombre 
-                      </button>
+                      <span class="fw-medium">Cambiar Nombre </span>
+                    </button>
                   </li>
 
-                  <li class="list-group-item w-auto text-start p-2 border border-0 fw-medium encima rounded-pill mb-2">
-                    <button type="button" class="btn" data-toggle="modal" data-target="#changePasswordModal">
+                  <li class="list-group-item border border-0 fw-medium encima rounded-pill mb-2">
+                    <button type="button" class="btn w-100 h-100 rounded-pill border border-0" data-toggle="modal" data-target="#changePasswordModal">
 
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock me-2" viewBox="0 0 16 16">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
                         <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
                       </svg>
                       
-                      Cambiar Contraseña
+                      <span class="fw-medium">Cambiar Contraseña</span>
                     </button>
                   </li>
 
