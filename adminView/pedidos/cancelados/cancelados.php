a3555ce5963ef1cba66c84070139975fe9fee925
wicho <luismahe2004@gmail.com>
2023-08-13 23:03:20 -0600

    saca el fue

--- adminView/pedidos/cancelados/cancelados.php
+++ adminView/pedidos/cancelados/cancelados.php
@@ -16,40 +16,42 @@ $pedidos = "SELECT * FROM vista_carrito_cancelado";
   <link rel="icon" type="image/png" sizes="32x32" href="../../icon.png">
   <link rel="stylesheet" href="../productos/estilo.css"/>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
   <style>
 
 
-    .container {
-      margin-top: 50px;
-    }
+        .container {
+          margin-top: 50px;
+        }
 
-    .table {
-      background-color: #fff;
-    }
+        .table {
+          background-color: #fff;
+        }
 
-    .sidebar .nav-item {
-      margin-bottom: 10px;
-      border-bottom: 1px solid #ddd;
-      padding-bottom: 5px;
-    }
+        .sidebar .nav-item {
+          margin-bottom: 10px;
+          border-bottom: 1px solid #ddd;
+          padding-bottom: 5px;
+        }
 
-    .sidebar {
-      height: 100vh;
-      background-color: #f8f9fa;
-    }
+        .sidebar {
+          height: 100vh;
+          background-color: #f8f9fa;
+        }
 
-    .sidebar .nav-link {
-      color: #333;
+        .sidebar .nav-link {
+          color: #333;
+        }
+        .product-image img {
+        max-width: 50px;
+        max-height: 50px;
+      }
+      .barra-deslizable{
+        overflow-y: auto;
+        /*position: relative;*/
     }
-    .product-image img {
-    max-width: 50px;
-    max-height: 50px;
-  }
-  .barra-deslizable{
-    overflow-y: auto;
-    /*position: relative;*/
-}
   </style>
+
   <title>VideoGame Store - Admin</title>
 </head>
 
@@ -58,53 +60,56 @@ $pedidos = "SELECT * FROM vista_carrito_cancelado";
   include '../encabesado.php';
   ?>
 
-<div class="d-flex justify-content-center mt-3 mb-3">
-        <a href="../pedidos.php" class="btn btn-primary mx-2">Pedidos</a>
-        <a href="#" class="btn btn-primary mx-2">Cancelados</a>
-        <a href="../pendientes/pendientes.php" class="btn btn-primary mx-2">Pendientes</a>
-        <a href="../completado/completados.php" class="btn btn-primary mx-2">Completados</a>
-    </div>
+      <div class="d-flex justify-content-center mt-3 mb-3">
+        <a href="../pedidos.php" class="btn btn-dark mx-2 fw-medium rounded-pill but">Pedidos</a>
+        <a href="#" class="btn btn-dark mx-2 fw-medium rounded-pill but text-warning">Cancelados</a>
+        <a href="../pendientes/pendientes.php" class="btn btn-dark mx-2 fw-medium rounded-pill but">Pendientes</a>
+        <a href="../completado/completados.php" class="btn btn-dark mx-2 fw-medium rounded-pill but">Completados</a>
+      </div>
 
-    <div class="container-fluid">
-      <form class="d-flex">
-        <input class="form-control me-2 light-table-filter" data-table="table_id" type="text" placeholder="Buscar">
-        <hr>
-      </form>
-    </div>
+      <div class="container-fluid">
+        <form class="d-flex w-50 mx-auto">
+          <input class="form-control me-2 light-table-filter rounded-pill border border-dark shadow" data-table="table_id" type="text" placeholder="Buscar">
+          <hr>
+        </form>
+      </div>
 
-    
-    <div class="container mt-3">
+    <div class="p-4">
+      <div class="table-responsive rounded-4 mt-3">
         <table class="table table-striped table_id">
-        <thead> 
-              <tr>
-                <th>#</th>
-                <th>ID de la Orden</th>
-                <th>Nombre del Cliente</th>
-                <th>Cantidad Total</th>
-                <th>Fecha y Hora</th>
-                <th>Estado</th>
-              </tr>
-            </thead>
-            <tbody>
-            <?php 
-              $resultado = $conn->query($pedidos); 
-              while($row= $resultado->fetch_assoc()){
+              <thead class="border border-dark"> 
+                <tr>
+                  <th class="bg-dark text-light">#</th>
+                  <th class="bg-dark text-light">ID de la Orden</th>
+                  <th class="bg-dark text-light">Nombre del Cliente</th>
+                  <th class="bg-dark text-light">Cantidad Total</th>
+                  <th class="bg-dark text-light">Fecha y Hora</th>
+                  <th class="bg-dark text-light">Estado</th>
+                </tr>
+              </thead>
+          
+              <tbody>
+                <?php 
+                  $resultado = $conn->query($pedidos); 
+                  while($row= $resultado->fetch_assoc()){
 
-                
-              ?> 
-              <tr>
-                <td ><?php echo $row ["id_carrito"]?></td>
-                <td><?php echo $row ["id_order"];?></td>
-                <td><?php echo $row ["nombre_cliente"];?></td>
-                <td>$<?php echo $row ["total"];?></td>
-                <td><?php echo $row ["fecha_venta"];?></td>
-                <td><strong><?php echo $row ["estado"]?></strong></td>
-              </tr>
-              <?php
-              }?>
-            </tbody>        
-          </table>
+                    
+                  ?> 
+                  <tr>
+                    <td ><?php echo $row ["id_carrito"]?></td>
+                    <td><?php echo $row ["id_order"];?></td>
+                    <td><?php echo $row ["nombre_cliente"];?></td>
+                    <td>$<?php echo $row ["total"];?></td>
+                    <td><?php echo $row ["fecha_venta"];?></td>
+                    <td><strong><?php echo $row ["estado"]?></strong></td>
+                  </tr>
+                  <?php
+                  }?>
+              </tbody>        
+        </table>
+      </div>      
     </div>
+
 
 
     <script src="../../js/buscador.js"></script>
--- adminView/pedidos/pendientes/editaModal.php
+++ adminView/pedidos/pendientes/editaModal.php
@@ -2,9 +2,8 @@
 <div class="modal fade" id="editaModal" tabindex="-1" aria-labelledby="editaModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header head-mod">
                 <h1 class="modal-title fs-5" id="editaModalLabel">Editar registro</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="actualiza.php" method="post" enctype="multipart/form-data">
@@ -23,8 +22,8 @@
 
 
                     <div class="d-flex justify-content-end pt2">
-                        <button type="button" class="btn btn-secondary me-1" data-bs-dismiss="modal">Cerrar</button>
-                        <button type="submit" class="btn btn-primary ms-1"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+                        <button type="button" class="btn btn-danger border border-3 border-dark shadow rounded-pill me-1" data-bs-dismiss="modal"><b>CERRAR</b></button>
+                        <button type="submit" class="btn btn-warning border border-dark border-3 shadow rounded-pill ms-1"><i class="fa-solid fa-floppy-disk"></i> <b>GUARDAR</b></button>
                     </div>
 
                 </form>
