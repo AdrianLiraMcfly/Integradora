1f857d430098be45ef900cfe215974d6f0706fa1
wicho <luismahe2004@gmail.com>
2023-08-13 22:06:51 -0600

    saca el fue

--- adminView/products/category/editaModal.php
+++ adminView/products/category/editaModal.php
@@ -2,9 +2,9 @@
 <div class="modal fade" id="editaModal" tabindex="-1" aria-labelledby="editaModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header head-mod">
                 <h1 class="modal-title fs-5" id="editaModalLabel">Editar registro</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <!--<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>-->
             </div>
             <div class="modal-body">
                 <form action="actualiza.php" method="post" enctype="multipart/form-data">
@@ -12,13 +12,13 @@
                     <input type="hidden" id="id" name="id">
 
                     <div class="mb-2">
-                        <label for="nombre" class="form-label">Nombre:</label>
+                        <label for="nombre" class="form-label fw-medium">Nombre:</label>
                         <input type="text" name="nombre" id="nombre" class="form-control form-control-sm" required>
                     </div>
 
                     <div class="d-flex justify-content-end pt2">
-                        <button type="button" class="btn btn-secondary me-1" data-bs-dismiss="modal">Cerrar</button>
-                        <button type="submit" class="btn btn-primary ms-1"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+                        <button type="button" class="btn btn-danger border border-3 border-dark shadow rounded-pill me-1" data-bs-dismiss="modal"><b>CERRAR</b></button>
+                        <button type="submit" class="btn btn-warning border border-dark border-3 shadow rounded-pill ms-1"><i class="fa-solid fa-floppy-disk"></i> <b>GUARDAR</b></button>
                     </div>
 
                 </form>
