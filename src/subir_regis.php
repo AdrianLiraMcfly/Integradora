c76f9bc495c61d6236b0eb7af3ed88f0071fc49b
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2023-08-16 21:22:15 -0600

    verificar usuario

--- src/subir_regis.php
+++ src/subir_regis.php
@@ -10,17 +10,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $pass = $_POST["pass"];
     $passre = $_POST["passre"];
-    $hashedpassword = password_hash($pass, PASSWORD_DEFAULT); // Corregir el nombre de la variable
+    $hashedpassword = password_hash($pass, PASSWORD_DEFAULT);
 
     // Validar que los inputs NO contengan los caracteres especiales < > / \
     if (contieneCaracteresEspeciales($nombre) ||
         contieneCaracteresEspeciales($email) ||
         contieneCaracteresEspeciales($pass) ||
         contieneCaracteresEspeciales($passre)) {
-        $mensajeError = "Valores no validos en alguno de los campos. No se admiten los siguientes valores < > / \ .";
+        $mensajeError = "Valores no válidos en alguno de los campos. No se admiten los siguientes valores < > / \ .";
         header("Location: ../sesiones/register.php?mensaje=" . urlencode($mensajeError));
         exit();
     } else {
+        // Verificar si el email ya existe en la base de datos
+        $stmt = $conn->prepare("SELECT COUNT(*) AS count FROM usuarios WHERE email = :email");
+        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result['count'] > 0) {
+            $mensajeAlerta = "Email o usuario en uso, favor de ingresar otro.";
+            header("Location: ../sesiones/register.php?mensaje=" . urlencode($mensajeAlerta));
+            exit();
+        }
+
         $token = bin2hex(random_bytes(5));
         try {
             $stmt = $conn->prepare("INSERT INTO usuarios (nombre, email, contraseña, id_rol, id_estado, token) VALUES (:nombre, :email, :hashedpassword, 2, 2, :token)");
@@ -34,14 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("Location: tokenemail.php?email=$email&token=$token");
             exit();
         } catch (PDOException $e) {
-            if ($e->getCode() == 23000) {
-                // Error de clave duplicada (email ya en uso)
-                $mensajeAlerta = "Email o usuario en uso, favor de ingresar otro.";
-                header("Location: ../sesiones/register.php?mensaje=".urlencode($mensajeAlerta));
-            } else {
-                // Otro tipo de error
-                echo "Error al guardar el registro: " . $e->getMessage();
-            }
+            echo "Error al guardar el registro: " . $e->getMessage();
         }
     }
 }
